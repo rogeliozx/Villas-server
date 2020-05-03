c5de3072c40c31daebdced99417f184d3f75daf6
rogelioltz <rogelioltz@gmail.com>
2020-05-02 21:53:31 -0700

    feat: getAllNameSeccions

--- controllers/RegisterVisit.js
+++ controllers/RegisterVisit.js
@@ -4,10 +4,26 @@ const Visits = require('../models/Visits');
 const registerVisit = (req, visit) => {
   const { codeHouse } = req.body;
 };
-const test = (req, res) => {
-  res.json({ test: 'true' });
-};
 
+
+const getAllNameSeccions=async (req,res)=>{
+  /*  try {
+     const data=await Seccions.find({},'name');
+     res.status(200).json({
+       succes: true,
+       data,
+     });
+   } catch (error) {
+     console.log(error);
+     res.status(500).json({
+       succes: false,
+       error,
+     });
+   } */
+ 
+   res.status(200).json({hola:'hola'})
+ 
+ }
 const takePicture = async (codeHouse, year) => {
   const rec = new Recorder({
     url:
@@ -29,5 +45,5 @@ const takePicture = async (codeHouse, year) => {
 
 module.exports = {
   takePicture,
-  test
+  getAllNameSeccions
 };
--- routes/RegisterVisit.js
+++ routes/RegisterVisit.js
@@ -3,5 +3,5 @@ const visit = express.Router();
 const pictureController=require('../controllers/RegisterVisit');
 
 visit.post('/register-visit',pictureController.takePicture);
-visit.get('/test',pictureController.test)
+visit.get('/all-names',pictureController.getAllNameSeccions)
 module.exports = visit;
