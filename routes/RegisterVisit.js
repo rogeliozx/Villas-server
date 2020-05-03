49a05245af26b283688ea8a25ef0cfcfc5405f46
rogelioltz <rogelioltz@gmail.com>
2020-05-02 21:45:16 -0700

    test:test

--- routes/RegisterVisit.js
+++ routes/RegisterVisit.js
@@ -3,5 +3,5 @@ const visit = express.Router();
 const pictureController=require('../controllers/RegisterVisit');
 
 visit.post('/register-visit',pictureController.takePicture);
-
+visit.get('/test',pictureController.test)
 module.exports = visit;
